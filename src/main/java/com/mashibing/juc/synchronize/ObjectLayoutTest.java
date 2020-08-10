696031b2dd8461c937436b299afcb581bdbb025c
hgq0916 <1286908118@qq.com>
2020-08-10 13:49:34 +0800

    synchronized锁升级过程

---
+++ src/main/java/com/mashibing/juc/synchronize/ObjectLayoutTest.java
@@ -0,0 +1,22 @@
+package com.mashibing.juc.synchronize;
+
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * @author gangquan.hu
+ * @Package: com.mashibing.juc.synchronize.ObjectLayoutTest
+ * @Description: TODO
+ * @date 2020/8/10 13:27
+ */
+public class ObjectLayoutTest {
+
+  public static void main(String[] args) throws InterruptedException {
+    Thread.sleep(5000);
+    Object o = new Object();
+    System.out.println(ClassLayout.parseInstance(o).toPrintable());
+   /* synchronized (o){
+      System.out.println(ClassLayout.parseInstance(o).toPrintable());
+    }*/
+  }
+
+}
