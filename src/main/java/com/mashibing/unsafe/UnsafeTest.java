dddfd1b62293da26d45b3e1bea08c376788af83c
hgq0916 <1286908118@qq.com>
2020-08-11 13:29:38 +0800

    volatile及CAS的使用

---
+++ src/main/java/com/mashibing/unsafe/UnsafeTest.java
@@ -0,0 +1,18 @@
+package com.mashibing.unsafe;
+
+import sun.misc.Unsafe;
+
+/**
+ * @author gangquan.hu
+ * @Package: com.mashibing.unsafe.UnsafeTest
+ * @Description: TODO
+ * @date 2020/8/11 13:20
+ */
+public class UnsafeTest {
+
+  public static void main(String[] args) {
+    Unsafe unsafe = Unsafe.getUnsafe();
+    //unsafe.com
+  }
+
+}
