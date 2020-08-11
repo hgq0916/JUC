2e94075de2ac27c513f745be6faf3bb433942704
hgq0916 <1286908118@qq.com>
2020-08-11 09:12:01 +0800

    synchronized锁升级过程

--- src/main/java/com/mashibing/synchronize/ObjectLayoutTest.java
+++ src/main/java/com/mashibing/synchronize/ObjectLayoutTest.java
@@ -11,12 +11,13 @@ import org.openjdk.jol.info.ClassLayout;
 public class ObjectLayoutTest {
 
   public static void main(String[] args) throws InterruptedException {
-    Thread.sleep(5000);
+    //
+    //Thread.sleep(5000);
     Object o = new Object();
     System.out.println(ClassLayout.parseInstance(o).toPrintable());
-   /* synchronized (o){
+    synchronized (o){
       System.out.println(ClassLayout.parseInstance(o).toPrintable());
-    }*/
+    }
   }
 
 }
---
+++ src/main/java/com/mashibing/synchronize/ObjectLayoutTest2.java
@@ -0,0 +1,39 @@
+package com.mashibing.synchronize;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * @author gangquan.hu
+ * @Package: com.mashibing.juc.synchronize.ObjectLayoutTest
+ * @Description: TODO
+ * @date 2020/8/10 13:27
+ */
+public class ObjectLayoutTest2 {
+
+  Object o = new Object();
+
+  public  void print(){
+    synchronized (o){
+      /*try {
+        TimeUnit.SECONDS.sleep(1);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }*/
+      System.out.println(Thread.currentThread().getName()+":"+ClassLayout.parseInstance(o).toPrintable());
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    TimeUnit.SECONDS.sleep(5);
+    ObjectLayoutTest2 olt = new ObjectLayoutTest2();
+    System.out.println("main:"+ClassLayout.parseInstance(olt.o).toPrintable());
+
+    new Thread(olt::print,"m1").start();
+    new Thread(olt::print,"m2").start();
+    //new Thread(olt::print,"m3").start();
+
+
+  }
+
+}
