d3119b04265ef7809c4876b6c00341ea4740c734
hgq0916 <1286908118@qq.com>
2020-08-11 14:48:15 +0800

    通过反射获取unsafe类的实例

--- src/main/java/com/mashibing/unsafe/UnsafeTest.java
+++ src/main/java/com/mashibing/unsafe/UnsafeTest.java
@@ -1,5 +1,6 @@
 package com.mashibing.unsafe;
 
+import java.lang.reflect.Field;
 import sun.misc.Unsafe;
 
 /**
@@ -10,9 +11,15 @@ import sun.misc.Unsafe;
  */
 public class UnsafeTest {
 
-  public static void main(String[] args) {
-    Unsafe unsafe = Unsafe.getUnsafe();
-    //unsafe.com
+  public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
+    //直接获取unsafe实例抛异常
+    //Unsafe unsafe = Unsafe.getUnsafe();
+    //通过反射获取unsafe类的实例
+    //Field declaredField = Unsafe.class.getDeclaredFields()[0];
+    Field declaredField = Unsafe.class.getDeclaredField("theUnsafe");
+    declaredField.setAccessible(true);
+    Unsafe unsafe = (Unsafe) declaredField.get(null);
+    System.out.println(unsafe);
   }
 
 }
