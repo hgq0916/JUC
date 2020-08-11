02a506d58edced29262bcaeeedda7a9f79a57a4f
hgq0916 <1286908118@qq.com>
2020-08-11 15:02:38 +0800

    通过反射获取unsafe类的实例及unsafe的使用

--- src/main/java/com/mashibing/unsafe/UnsafeTest.java
+++ src/main/java/com/mashibing/unsafe/UnsafeTest.java
@@ -16,10 +16,59 @@ public class UnsafeTest {
     //Unsafe unsafe = Unsafe.getUnsafe();
     //通过反射获取unsafe类的实例
     //Field declaredField = Unsafe.class.getDeclaredFields()[0];
-    Field declaredField = Unsafe.class.getDeclaredField("theUnsafe");
-    declaredField.setAccessible(true);
-    Unsafe unsafe = (Unsafe) declaredField.get(null);
-    System.out.println(unsafe);
+
+    //操作基本类型的变量
+    operateBasicVar();
+    //操作线程
+    operateThread();
+    //cas
+    cas();
   }
 
+  private static void cas() throws NoSuchFieldException {
+    Data data = new Data();
+    data.a = 10;
+    data.b = false;
+    Unsafe unsafe = getUnsafe();
+    Field fieldA = Data.class.getDeclaredField("a");
+    fieldA.setAccessible(true);
+    long address = unsafe.objectFieldOffset(fieldA);
+    unsafe.compareAndSwapInt(data,address,10,20);
+    System.out.println("data.a:"+data.a);
+  }
+
+  private static void operateThread() throws NoSuchFieldException {
+
+  }
+
+  private static void operateBasicVar() {
+    Unsafe unsafe = getUnsafe();
+    long address = unsafe.allocateMemory(4L);
+    unsafe.putInt(address,10);
+    System.out.println(unsafe.getInt(address));
+    unsafe.freeMemory(address);
+    System.out.println(unsafe.getAddress(address));
+  }
+
+  public static Unsafe getUnsafe(){
+    Field declaredField = null;
+    try {
+      declaredField = Unsafe.class.getDeclaredField("theUnsafe");
+      declaredField.setAccessible(true);
+      Unsafe unsafe = (Unsafe) declaredField.get(null);
+      return unsafe;
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+
+    return null;
+  }
+
+}
+
+class Data {
+  int a;
+  boolean b;
 }
