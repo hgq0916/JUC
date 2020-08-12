37832b61b837655179120c4f12e359c6ab2b2aa8
hgq0916 <1286908118@qq.com>
2020-08-12 09:58:30 +0800

    AtomicXXX、ReentrantLock、ReentrantLock与synchronized对比、CountDownLatch的使用

--- src/main/java/com/mashibing/atomic/AtomicLongTest.java
+++ src/main/java/com/mashibing/atomic/AtomicLongTest.java
@@ -16,13 +16,13 @@ public class AtomicLongTest {
   }
 
   public void longAdder(){
-    for(int i=0;i<10000;i++){
+    for(int i=0;i<100000;i++){
       longAdder.increment();
     }
   }
 
   public synchronized void synchronizedTest(){
-    for(int i=0;i<10000;i++){
+    for(int i=0;i<100000;i++){
       count++;
     }
   }
